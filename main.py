0844134a9a3925e35cbf92407fe602f101328072
savinov2 <savinov2@mail.ru>
2023-05-20 13:37:14 +0300

    add create ADVISEMENT

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import Message
 import logging
 
@@ -10,7 +11,7 @@ import logging
 
 TOKEN = '5817538532:AAEjWn_okKQe0C9I5nwsV_pbBYXDE5BdObI'
 
-ADMIN = 1328821049
+ADMIN = 22
 
 kb_admin = types.ReplyKeyboardMarkup(resize_keyboard=True)
 kb_admin.add(types.InlineKeyboardButton(text="Показать заявки"))
@@ -18,10 +19,16 @@ kb_admin.add(types.InlineKeyboardButton(text="Показать заявки"))
 kb_user = types.ReplyKeyboardMarkup(resize_keyboard=True)
 kb_user.add(types.InlineKeyboardButton(text="Создать заявку"))
 
+
 logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.INFO, filename = u'log/mylog.log')
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot=bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot=bot, storage=storage)
+
+class dialog(StatesGroup):
+    creating_application = State()
+
 
 # class dialog(StatesGroup):
 # 	spam = State()
@@ -67,6 +74,7 @@ async def sart(message: types.Message):
     else: 
         logging.info(f'User {id=} started {user_full_name=}')
         await message.answer((f'Привет, {user_full_name}'),reply_markup=kb_user)
+        await dialog.creating_application.set()
         
 @dp.message_handler(text="Показать заявки", user_id=ADMIN)
 async def show_applications(message: types.Message):
@@ -94,6 +102,44 @@ async def show_applications(message: types.Message):
         logging.error(f"Error while fetching applications: {str(e)}")
         await message.answer("Произошла ошибка при получении списка заявок.")
 
+@dp.message_handler(text="Создать заявку", state="*")
+async def create_application(message: types.Message, state: FSMContext):
+    await message.answer("Введите данные для заявки.")
+    await message.answer("В таком формате (Имя,Фамилия,Группа,Курс).")
+    await dialog.creating_application.set()
+
+@dp.message_handler(state=dialog.creating_application)
+async def process_application_data(message: types.Message, state: FSMContext):
+    try:
+        data = message.text.split(',')
+        name = data[0].strip()
+        surname = data[1].strip()
+        grp = data[2].strip()
+        course = data[3].strip()
+
+        conn = db_connect()
+        if conn is None:
+            await message.answer("Ошибка подключения к базе данных.")
+            return
+
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO advisement (state, name, surname, grp, course) VALUES (FALSE, %s, %s, %s, %s)",
+            (name, surname, grp, course)
+        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        await message.answer("Заявка успешно создана.")
+
+        await state.finish()  # Завершаем состояние dialog.creating_application
+    except Exception as e:
+        logging.error(f"Error while processing application data: {str(e)}")
+        await message.answer("Произошла ошибка при создании заявки.")
+
+        await state.finish()  # Завершаем состояние dialog.creating_application
+    
     
 if __name__ == '__main__':
     executor.start_polling(dp)
